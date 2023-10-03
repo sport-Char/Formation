4f8b063a21d890abbcd8e07c2dc1c9a3c6084708
sport-Char <126862108+sport-Char@users.noreply.github.com>
2023-10-03 15:30:22 +0200

    Update appstreamlit.py

--- appstreamlit.py
+++ appstreamlit.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 
 def getWinner(score):
     numbers = score.split(':')  # Divisez la première partie en deux nombres: '2' et '5'
@@ -38,8 +37,13 @@ def addPoint(df):
     return df
 
 def createPieChart(df):
-    colors = sns.color_palette('pastel')[0:len(df['Résultat'].unique())]
+    plt.rcParams['font.sans-serif'] = 'Arial'
+    custom_colors = {'Egalité': '#4861db', 'Victoire': '#8cd071', 'Défaite': '#ec4f42'}
+
+
     result_counts = df['Résultat'].value_counts()
+
+    colors = [custom_colors.get(value, sns.color_palette('pastel')[i]) for i, value in enumerate(result_counts.index)]
     fig, ax = plt.subplots()
     ax.pie(result_counts, labels=result_counts.index, autopct='%1.1f%%', startangle=90, colors=colors)
     ax.axis('equal')  # Assurez-vous que le graphique est un cercle parfait
@@ -59,7 +63,7 @@ if uploaded_file is not None:
     compoB = st.sidebar.multiselect("Choix formation équipe B",df["compoB"].unique())
     st.sidebar.title("Choisir les continents")
     continents = st.sidebar.multiselect("Choix continent(s)", df["Continent"].unique())
-    influence = st.sidebar.slider("Affluence",0,200,max(df["influence"]))
+    influence = st.sidebar.slider("Affluence",0,max(df["influence"]),value=0)
     if (len(compoA)>0) & (len(compoB)>0):
         if len(continents)==0:
             new_df = df[(((df['compoA'].isin(compoA)) & (df['compoB'].isin(compoB))) | ((df['compoB'].isin(compoA)) & (df['compoA'].isin(compoB)))) & (df["influence"] >= influence)]
